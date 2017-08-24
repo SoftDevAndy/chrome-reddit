18c63bbb70a1acec1d33cc63476d68a1f876b8b3
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2017-08-24 16:42:33 -0400

    All of the code, all 5-6 lines of it!

---
+++ assets/js/cleaner.js
@@ -0,0 +1,8 @@
+var contentToRemove = document.querySelectorAll("#siteTable_organic");
+$(contentToRemove).remove(); 
+
+contentToRemove = document.querySelectorAll(".side");
+$(contentToRemove).remove();
+
+contentToRemove = document.querySelectorAll(".footer-parent");
+$(contentToRemove).remove();
